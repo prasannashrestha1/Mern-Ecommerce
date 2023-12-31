927adf54da1038a3f85e1853cab820c3da1593b0
Prasanna Shrestha <shresthaprasanna230@gmail.com>
2023-12-31 23:25:11 +0545

    performing crud in category and product page

--- clients/src/pages/Admin/CreateCategory.jsx
+++ clients/src/pages/Admin/CreateCategory.jsx
@@ -38,8 +38,8 @@ const CreateCategory = () => {
       const { data } = await axios.get(
         "http://localhost:8080/api/v1/category/get-category"
       );
-      if (data.success) {
-        setCategories(data.category);
+      if (data?.success) {
+        setCategories(data?.category);
       }
     } catch (error) {
       console.log(error);
--- clients/src/pages/Admin/CreateProducts.jsx
+++ clients/src/pages/Admin/CreateProducts.jsx
@@ -1,8 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu";
+import toast from "react-hot-toast";
+import axios from "axios";
+import { Select } from "antd";
+import { useNavigate } from "react-router-dom";
+
+const { Option } = Select;
 
 const CreateProducts = () => {
+  const navigate = useNavigate();
+  const [categories, setCategories] = useState([]);
+  const [photo, setPhoto] = useState("");
+  const [names, setNames] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [shipping, setShipping] = useState("");
+
+  //get all category
+  const getAllCategories = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/category/get-category"
+      );
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting category");
+    }
+  };
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
+  //create product function
+  const handleCreate = async (e) => {
+    e.preventDefault();
+    try {
+      const productData = new FormData();
+      productData.append("names", names);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("quantity", quantity);
+      productData.append("photo", photo);
+      productData.append("category", category);
+      const { data } = axios.post(
+        "http://localhost:8080/api/v1/product/create-product",
+        productData
+      );
+      if (data?.success) {
+        toast.error(data?.message);
+      } else {
+        toast.success(`Product Created Succcessfully`);
+        navigate("/dashboard/admin/products");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while submitting");
+    }
+  };
   return (
     <Layout title={"Dashboard - Create Product"}>
       <div className="w-[100%]">
@@ -11,6 +72,134 @@ const CreateProducts = () => {
             <AdminMenu />
           </div>
           <div className="col-span-9 w-24">Create Products</div>
+          <div className="m-1 w-75">
+            <Select
+              bordered={false}
+              placeholder="Select a category"
+              size="large"
+              showSearch
+              className="mb-3"
+              onChange={(value) => {
+                setCategory(value);
+              }}
+            >
+              {categories?.map((c) => (
+                <Option key={c._id} value={c._id}>
+                  {c.name}
+                </Option>
+              ))}
+            </Select>
+            <div className="mb-3">
+              <button className="bg-slate-200 p-3 rounded-lg hover:bg-slate-300 hover:text-blue-500">
+                <label>
+                  {photo ? photo.name : "Upload Photo"}
+                  <input
+                    type="file"
+                    name="photo"
+                    accept="image/*"
+                    onChange={(e) => setPhoto(e.target.files[0])}
+                    hidden
+                  />
+                </label>
+              </button>
+            </div>
+            <div className="mb-3">
+              {photo && (
+                <div className="text-center">
+                  <img
+                    src={URL.createObjectURL(photo)}
+                    alt="product photo"
+                    height={"200px"}
+                    className="w-[50%] rounded-xl"
+                  />
+                </div>
+              )}
+            </div>
+            <div className="mb-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                value={names}
+                placeholder="Write a Name"
+                className="shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={(e) => setNames(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="Desciption"
+              >
+                Descrption
+              </label>
+              <input
+                type="text"
+                value={description}
+                placeholder="Write a Description"
+                className="shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="price"
+              >
+                Price
+              </label>
+              <input
+                type="text"
+                value={price}
+                placeholder="Write a Price"
+                className="shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="Quantity"
+              >
+                Quantity
+              </label>
+              <input
+                type="text"
+                value={quantity}
+                placeholder="select your quantity"
+                className="shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={(e) => setQuantity(e.target.value)}
+              />
+            </div>
+            <div className="mb-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="shipping"
+              >
+                Shipping
+              </label>
+              <Select
+                bordered={false}
+                placeholder="Select Shipping"
+                size="large"
+                showSearch
+                className="shadow appearance-none border rounded w-[50%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                onChange={(e) => setShipping(e.target.value)}
+              >
+                <Option value="0">No</Option>
+                <Option value="1">Yes</Option>
+              </Select>
+            </div>
+            <div className="mb-3" onClick={handleCreate}>
+              <button className="w-[50%] bg-slate-200 p-3 my-9">
+                Create Product
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
